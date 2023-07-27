aafc73a7951fd2409f29a4b41f77de3f199ed80e
Martin Johnsson <sorill@gmail.com>
2023-07-27 20:08:44 +0200

    Add c++ for speed

---
+++ R/simsimqt-package.R
@@ -0,0 +1,7 @@
+## usethis namespace: start
+#' @useDynLib simsimqt, .registration = TRUE
+## usethis namespace: end
+
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+## usethis namespace: end
